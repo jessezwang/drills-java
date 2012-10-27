0cf4867d44cd53bd5ec6b7acd5aacb2fa3cf1a3a
Zhe Wang <jesse.zhe.wang@gmail.com>
2012-10-26 21:30:19 -0400

    bfs editioin of Unique Path II.

--- src/UniquePaths2.java
+++ src/UniquePaths2.java
@@ -1,23 +1,29 @@
-
+import java.util.*;
+import java.awt.Point;
 public class UniquePaths2 {
-	 private int dfs(int x, int y, int[][] obstacleGrid){
-	        if (1==obstacleGrid[0][0]) return 0;
-	        if (x==obstacleGrid.length-1 && y==obstacleGrid[x].length-1 && obstacleGrid[x][y]==0) return 1;
-	        if (x<obstacleGrid.length-1 && obstacleGrid[x+1][y]==0 && y<obstacleGrid[x].length-1 && obstacleGrid[x][y+1]==0) 
-	            return dfs(x+1,y, obstacleGrid)+dfs(x,y+1, obstacleGrid);
-	        if (x<obstacleGrid.length-1 && obstacleGrid[x+1][y]==0) return dfs(x+1,y, obstacleGrid);
-			if (y<obstacleGrid[x].length-1 && obstacleGrid[x][y+1]==0) return dfs(x,y+1, obstacleGrid);
-			return 0;
-		}
-		public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-			return dfs(0, 0, obstacleGrid);
-	    }
-	/**
-	 * @param args
-	 */
+	public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if (obstacleGrid[0][0]==1) return 0;
+        int x=0, y=0, count=0;
+        Queue<Point> queue = new LinkedList<Point>();
+        queue.offer(new Point(0,0));
+        while(!queue.isEmpty()){
+        	Point p = queue.poll();
+        	x=p.x;
+        	y=p.y;
+        	if (x==obstacleGrid.length-1 && y==obstacleGrid[x].length-1) count++;
+        	else{
+        		if(x<obstacleGrid.length-1 && obstacleGrid[x+1][y]==0)
+        			queue.offer(new Point(x+1,y));
+        		if(y<obstacleGrid[x].length-1 && obstacleGrid[x][y+1]==0)
+        			queue.offer(new Point(x, y+1));
+        	}
+        }
+        return count;    
+    }	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		UniquePaths2 up = new UniquePaths2();
+		System.out.println(up.uniquePathsWithObstacles(new int[][]{{0},{0}}));
+		
 	}
-
 }
