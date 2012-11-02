0c5015eb0a0b0aa3f2597d2678e2dc07b475fc84
Zhe Wang <jesse.zhe.wang@gmail.com>
2012-11-01 22:30:19 -0400

    TrieST, time out again...

--- src/FindingStrings.java
+++ src/FindingStrings.java
@@ -7,104 +7,74 @@ import java.io.FileNotFoundException;
 import java.util.*;
 
 public class FindingStrings {
-	public static class TST{
-		private static class Node
-		{
-		   char c;
-		   Node left, mid, right;
-		   long val;
-		}
-		private static Node root = null;
+	public static class TrieST{
+		private static int R = 26; // radix
+	    private static Node root;          // root of trie
+	     
+		private static class Node {
+	        private long val;
+	        private Node[] next = new Node[R];
+	    }
 		
 		public static String get(long k){
 			return get(root, k, new String());
-		}
-		
-		public static Node get(String key) {
-			return get(root, key, 0);
-		}
-		
-		private static Node get(Node x, String key, int d)
-		{
-		   if (x == null) return null;
-		   char c = key.charAt(d);
-		   if      (c < x.c) return get(x.left,  key, d);
-		   else if (c > x.c) return get(x.right, key, d);
-		   else if (d < key.length() - 1) return get(x.mid, key, d+1);
-		   else return x;
-		}		
-		
+		}	
 		private static String get(Node x, long k, String part){
-			class NodeComparator implements Comparator{
-			    public int compare(Object o1, Object o2) {
-			        Node p1 = (Node) o1;
-			        Node p2 = (Node) o2; 
-			        return p1.c - p2.c;
-			    }
-			}
-			Map<Node, Long> values = new HashMap<Node, Long>();
-			Queue<Node> candidates = new LinkedList<Node>();
-			candidates.offer(x);
-			values.put(x, x.val);
-			while(!candidates.isEmpty()){
-				x = candidates.poll();
-				if (x.left!=null) {
-					candidates.offer(x.left);
-					values.put(x.left, x.left.val);
-				}
-				if (x.right!=null) {
-					candidates.offer(x.right);
-					values.put(x.right, x.right.val);
-				}
-			}	
-			List<Object> list = new ArrayList<Object>(values.keySet());
-		    Object[] objects = list.toArray();
-
-			Arrays.sort(objects, new NodeComparator());
-
-//			for (Object obj: objects){
-//				Node n = (Node)obj;
-//				System.out.println(n.c);
+//			for (int i=0; i<R; i++){
+//				if (x.next[i]!=null) System.out.println(x.next[i].val);
 //			}
-			Node n = null;
-			for (Object obj: objects){
-				n = (Node)obj;
-//				System.out.print(n.c);
-//				System.out.print(k);
-//				System.out.println();
-				if (k==1) return new String(part+n.c);
-				if (k>n.val) k-=n.val; 
-				else return get(n.mid, k-1, part+n.c);
+//			System.out.println();
+			for (int i=0; i<R; i++){
+				if (x.next[i]==null) continue;
+//				System.out.println((char)('a'+i));
+//				System.out.println(x.val);
+//				System.out.println(k);
+				if (k==0) return part+(char)('a'+i);
+				if (k>x.next[i].val) k-=x.next[i].val;
+				else return get(x.next[i], k-1, part+(char)('a'+i));
 			}
-			return new String("INVALID");
+			if (part.isEmpty()) return "INVALID";
+			return part;
 		}
 		
-		public static void put(String key){
+		public static void put(String key)
+	    {
 			root = put(root, key, 0);  
 		}
+		private static Node put(Node x, String key, int d)
+	    {  // Change value associated with key if in subtrie rooted at x.
+	        if (x == null) x = new Node();
+	        x.val++;
+	        if (d == key.length()) {  return x; }
+	        char c = key.charAt(d); // Use dth key char to identify subtrie.
+	        x.next[c-'a'] = put(x.next[c-'a'], key, d+1);
+	        return x;
+	    }
 		
-		public static boolean check(String key){
-			return check(root, key, 0);
-		}
+//		public static boolean check(String key){
+//			return check(root, key, 0);
+//		}
 		
-		private static boolean check(Node x, String key, int d){
-			char c = key.charAt(d);
-			if (x == null) return false;
-			if      (c < x.c) return check(x.left,  key, d);
-			else if (c > x.c) return check(x.right, key, d);
-			else if (d < key.length() - 1) return check(x.mid, key, d+1); 
-		   	else return true;
-		}
-		
-		private static Node put(Node x, String key, int d){
-		   char c = key.charAt(d);
-		   if (x == null) { x = new Node(); x.c = c; }
-		   if      (c < x.c) x.left  = put(x.left,  key, d);
-		   else if (c > x.c) x.right = put(x.right, key, d);
-		   else if (d < key.length() - 1) {x.mid = put(x.mid, key, d+1); x.val++;}
-		   else x.val++;
-		   return x; 
-		}
+//		private static boolean check(Node x, String key, int d){
+//			char c = key.charAt(d);
+//			if (d == key.length()) return 
+//			if (x == null) return false;
+//			if      (x.next[c-'a']!=null) return check(x.next[c-'a'],  key, d);
+//			else if (d < key.length() - 1) return check(x.mid, key, d+1); 
+//		   	else return true;
+//		}
+//		
+//		private static Node put(Node x, String key, int d){
+//		   char c = key.charAt(d);
+//		   if (x == null) { x = new Node(); }
+//		   if (d == key.length()) {  x.val = val; return x; }
+//		   
+//		   
+//		   else if (c > x.c) x.right = put(x.right, key, d);
+//		   else if (d < key.length() - 1) {x.mid = put(x.mid, key, d+1); x.val++;}
+//		   else x.val++;
+//		   return x; 
+//		}
 	};
 		
 	/**
@@ -112,21 +82,33 @@ public class FindingStrings {
 	 * @throws FileNotFoundException 
 	 */
 	public static void main(String[] args) throws FileNotFoundException {
-		Scanner in = new Scanner(System.in);
-//		Scanner in = new Scanner(new File("src/input00.txt"));
+//		Scanner in = new Scanner(System.in);
+		Scanner in = new Scanner(new File("src/input00.txt"));
 		int n = in.nextInt();
+		String[] strings = new String[n];
+		int len = 0;
+		HashSet<String> set = new HashSet<String>();
 		for (int i=0; i<n; i++){
-			
-			String s = in.next();
-			for(int sz=1; sz<=s.length(); sz++)
-				for (int beg=0; beg<s.length()-sz+1; beg++)
-					if (!TST.check(s.substring(beg, beg+sz)) )
-							TST.put(s.substring(beg, beg+sz));
+			strings[i] = in.next();
+			if (strings[i].length()>len) len=strings[i].length();
 		}
+		
+		for(int sz=1; sz<=len; sz++){
+			set.clear();
+			for (String s: strings){
+				if (s.length()<sz) continue;
+				for (int beg=0; beg<s.length()-sz+1; beg++)
+					if (!set.contains(s.substring(beg, beg+sz)) ){
+							TrieST.put(s.substring(beg, beg+sz));
+							set.add(s.substring(beg,beg+sz));
+					}
+			}				
+		}
+		
 		int q = in.nextInt();
 		for (int j=0; j<q; j++) {
 			int k = in.nextInt();
-			System.out.println( TST.get(k) );
+			System.out.println( TrieST.get(k) );
 		}
 	}
 
